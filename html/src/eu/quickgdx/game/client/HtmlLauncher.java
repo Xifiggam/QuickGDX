fc548fa285906f1af5a9952b1f47794c59bc6d24
Xifiggam <xifiggam@gmx.at>
2016-08-24 21:23:51 +0200

    Inital commit

---
+++ html/src/eu/quickgdx/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package eu.quickgdx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import eu.quickgdx.game.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxGame();
+        }
+}
