28c8aa4a21bb481e5e58dcb859347641748ece1d
Xifiggam <xifiggam@gmx.at>
2016-08-24 21:31:14 +0200

    Commit test

--- desktop/src/eu/quickgdx/game/desktop/DesktopLauncher.java
+++ desktop/src/eu/quickgdx/game/desktop/DesktopLauncher.java
@@ -8,5 +8,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
+		System.out.println("Test change.");
 	}
 }
